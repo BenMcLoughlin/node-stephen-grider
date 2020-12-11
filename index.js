9641c728acaba2bb4f7474d429eb500a5f13af65
Ben McLoughlin <benmcl@shaw.ca>
2020-12-11 07:34:41 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+     res.send({hi: "there"})
+})
+
+app.get('/greeting', (req, res) => {
+     res.send({hi: "greeeting"})
+})
+
+const PORT = process.env.port || 5000
+
+app.listen(PORT)
